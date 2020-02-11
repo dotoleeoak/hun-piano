7dda4f751480f5fdf6b2b9f292f7e60cccb91e7e
Kim Jeong-Won <threedalpeng@gmail.com>
2020-02-12 01:54:41 +0900

    Update HunPianoManager.py

--- PianoManager/HunPianoManager.py
+++ PianoManager/HunPianoManager.py
@@ -2,39 +2,56 @@ import sys
 from PySide2.QtWidgets import QApplication, QMainWindow, QStackedWidget
 from main_LogIn import LogIn
 from main_SignUp import SignUp
+from main_InUse import InUse
 
 class MainWindow(QMainWindow):
+    
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("Piano Checker")
         self.setFixedSize(800, 480)
-        self.showFullScreen()
+        #self.showFullScreen()
 
         # Create Page Objects Here
         LogInPage = LogIn()
         SignUpPage = SignUp()
+        InUsePage = InUse()
 
         # Connect pages to switch each others
         LogInPage.ui.ButtonRegister.clicked.connect(self.showSignUpPage)
+        LogInPage.ui.DButtonYes.clicked.connect(self.showInUsePage)
         SignUpPage.ui.buttonHome.clicked.connect(self.showLogInPage)
+        InUsePage.ui.pushButton.clicked.connect(self.showLogInPage)
 
         # Insert pages into QStackedWidget
         self.centralWidgets = QStackedWidget()
         self.centralWidgets.addWidget(LogInPage)    # index: 0
         self.centralWidgets.addWidget(SignUpPage)   # index: 1
+        self.centralWidgets.addWidget(InUsePage)    # index: 2
 
         self.setCentralWidget(self.centralWidgets)
 
-    def showSignUpPage(self):
-        print("signup")
-        self.centralWidgets.setCurrentIndex(1)
+    def getCurWidget(self):
+        return self.centralWidgets.currentWidget()
 
     def showLogInPage(self):
         print("login")
         self.centralWidgets.setCurrentIndex(0)
+        self.getCurWidget().ui.DialogueShadow.hide()
         
 
-class PianoChecker(QApplication):
+    def showSignUpPage(self):
+        print("signup")
+        self.centralWidgets.setCurrentIndex(1)
+        
+
+    def showInUsePage(self):
+        print("inuse")
+        self.centralWidgets.setCurrentIndex(2)
+
+        
+
+class PianoManager(QApplication):
     def __init__(self, argv):
         QApplication.__init__(self, argv)
 
@@ -44,5 +61,5 @@ class PianoChecker(QApplication):
 
 if __name__ == '__main__':
     
-    app = PianoChecker(sys.argv)
+    app = PianoManager(sys.argv)
     sys.exit(app.exec_())
