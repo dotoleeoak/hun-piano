81e7ba2bcac8c16af80f473abac9c555a08869dd
Kim Jeong-Won <threedalpeng@gmail.com>
2020-02-17 18:59:48 +0900

    Add NFC Module
    MFRC522: NFC 라이브러리
    NFCReader, NFCReaderForTest:
    NFC 신호 읽어오는 모듈(스레드로 구현)

---
+++ PianoManager/NFC/NFCReaderForTest.py
@@ -0,0 +1,32 @@
+from PySide2.QtCore import QThread, Signal
+
+
+class NFCReader(QThread):
+    nfc_connect = Signal(str)
+    reading = True
+    
+    def __init__(self):
+        super().__init__()
+        
+
+    def run(self):
+        
+        print("Thread running")
+        while self.reading:
+            self.msleep(1000)
+            pass
+
+    def start(self):
+        self.reading = True
+        super().start()
+                    
+    def quit(self):
+        self.reading = False
+        super().quit()
+            
+    def uidToString(uid):
+        uid_str = ""
+        for i in uid:
+            uid_str = format(i, '02X') + uid_str
+        return uid_str
+            
