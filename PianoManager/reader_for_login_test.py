8361e74710ebe89dde677ccafdf57b79210cd3ba
Kim Jeong-Won <threedalpeng@gmail.com>
2020-02-17 18:58:24 +0900

    Update reader_for_login_test.py
    UID 탐색 추가
    함수 이름과 탐색 방식 변경

--- PianoManager/reader_for_login_test.py
+++ PianoManager/reader_for_login_test.py
@@ -14,28 +14,46 @@ class TestDatabaseReader:
         # 데이터 방식이 어떨지 모르지만 임의로 만든 예시 데이터.
         # 세계적인 피아니스트가 꿈인 진영이의 연습량을 표현해보았다.
         # 평범한 김정원과 비교해보자.
-        self.data = {
-            "12345678":
+        self.data = [
             {
+                "ID": "12345678",
                 "name": "이진영",
-                "time_used" : "17:01:25"
+                "time_used" : "17:01:25",
+                "NFC_UID" : None
             },
-            "11111112":
             {
+                "ID": "11111112",
                 "name": "김정원",
-                "time_used": "00:30:12"
-            }}
+                "time_used": "00:30:12",
+                "NFC_UID" : "81FCF24F"
+            }]
 
-        self.currentData = {"name": "None", "time_used": "00:00:00"}
+        self.currentData = {
+            "ID": "-1",
+            "name": "None",
+            "time_used": "00:00:00",
+            "NFC_UID": "-1"
+            }
 
 
-    def passInDatabase(self, password):
+    def isPassInDatabase(self, password):
 
-        if password in self.data.keys():
-            self.currentData = self.data[password]
-            return (True, self.currentData)
-        else:
+        data_found = next((item for item in self.data if item["ID"] == password), False)
+        print(data_found)
+        if not data_found:
             return (False, None)
+        else:
+            self.currentData = data_found
+            return (True, self.currentData)
+
+    def isUidInDatabase(self, uid):
+
+        data_found = next((item for item in self.data if item["NFC_UID"] == uid), False)
+        if not data_found:
+            return (False, None)
+        else:
+            self.currentData = data_found
+            return (True, self.currentData)
         
 
     def getCurrentData(self):
