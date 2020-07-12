550984dcd9c7ca03a117aefe1ae4c9a2ab66a5a4
Jaemin Choi <1dotolee@gmail.com>
2020-07-13 02:09:42 +0900

    Use absolute path and arrange some code
    이미지 불러올 때 절대경로 적용
    StyleSheet frame 통한 상속
    그 외 사소한 내용 수정

--- src/UI/InUse.py
+++ src/UI/InUse.py
@@ -1,40 +1,54 @@
-from PySide2.QtCore import QCoreApplication, QRect, Qt
-from PySide2.QtGui import QFont, QPixmap
-from PySide2.QtWidgets import QFrame, QLabel, QPushButton
+import os
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from .path import PATH_IMG
 
 
 class UiInUse:
     def setupUi(self, widget):
         widget.resize(800, 480)
 
+        # TODO: Rename variables of InUse, LogIn, and SignUp to be consistent
+        # Example: frame => frame_smth, label => label_smth
+
         self.background_frame = QFrame(widget)
         self.background_frame.setGeometry(QRect(0, 0, 800, 480))
-        self.background_frame.setStyleSheet("background-color: rgb(241, 168, 23);")
+        self.background_frame.setStyleSheet(
+            "QFrame {"
+            "   background-color: rgb(241, 168, 23);"
+            "}"
+            "QLabel {"
+            "   font: 20pt 배달의민족 주아;"
+            "   color: white;"
+            "}"
+        )
 
         self.piano_picture = QLabel(self.background_frame)
         self.piano_picture.setGeometry(QRect(320, 30, 160, 150))
-        self.piano_picture.setPixmap(QPixmap("../../Images/Piano.png"))
+        self.piano_picture.setPixmap(QPixmap(os.path.join(PATH_IMG, "Piano.png")))
 
         self.used_time = QLabel(self.background_frame)
         self.used_time.setGeometry(QRect(0, 180, 800, 40))
-        self.used_time.setStyleSheet("font: 16pt 배달의민족 주아; color: white;")
+        self.used_time.setStyleSheet("font-size: 16pt;")
         self.used_time.setText("01:30:10")
         self.used_time.setAlignment(Qt.AlignCenter)
 
         self.username = QLabel(self.background_frame)
         self.username.setGeometry(QRect(0, 230, 800, 41))
-        self.username.setStyleSheet("font: 20pt 배달의민족 주아; color: white;")
+        self.username.setStyleSheet("font-size: 20pt;")
         self.username.setText("홍길동")
         self.username.setAlignment(Qt.AlignCenter)
 
         self.in_use = QLabel(self.background_frame)
         self.in_use.setGeometry(QRect(0, 270, 800, 71))
-        self.in_use.setStyleSheet("font: 32pt 배달의민족 주아; color: white;")
+        self.in_use.setStyleSheet("font-size: 32pt;")
         self.in_use.setText("사용 중")
         self.in_use.setAlignment(Qt.AlignCenter)
 
         self.button_quit = QPushButton(self.background_frame)
         self.button_quit.setGeometry(QRect(340, 360, 120, 60))
+        self.button_quit.setText("종료")
         self.button_quit.setStyleSheet(
             "QPushButton { "
             "   font: 20pt 배달의민족 주아; "
@@ -47,4 +61,3 @@ class UiInUse:
             "   background-color: rgb(120, 0, 0); "
             "} "
         )
-        self.button_quit.setText("종료")
--- src/UI/LogIn.py
+++ src/UI/LogIn.py
@@ -1,9 +1,8 @@
-# from PySide2.QtCore import QRect, QSize, Qt
-# from PySide2.QtGui import QBrush, QColor, QIcon, QPalette, QPixmap
-# from PySide2.QtWidgets import QFrame, QPushButton
+import os
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
+from .path import PATH_IMG
 
 
 class UiLogIn:
@@ -13,19 +12,17 @@ class UiLogIn:
         self.frame_keypad = QFrame(widget)
         self.frame_keypad.setGeometry(QRect(116, 275, 568, 180))
         self.frame_keypad.setStyleSheet(
-            "QPushButton { "
-            "   font: 36pt 배달의민족 주아; "
-            "   color: rgb(255, 188, 50); "
-            "   background-color: white; "
-            "   border-style: outset; "
-            "   border-width: 5px; "
-            "   border-radius: 12px; "
-            "   border-color: rgb(225, 158, 20); "
-            "} "
-            "QPushButton:hover { "
-            "   background-color: rgb(223, 163, 43); "
-            "   color: white; "
-            "} "
+            "QPushButton {"
+            "   font: 36pt 배달의민족 주아;"
+            "   color: rgb(255, 188, 50);"
+            "   background-color: white;"
+            "   border: 5px outset rgb(255, 158, 20);"
+            "   border-radius: 16px;"
+            "}"
+            "QPushButton:hover {"
+            "   background-color: rgb(223, 163, 43);"
+            "   color: white;"
+            "}"
         )
 
         self.button_keypad = []
@@ -38,50 +35,54 @@ class UiLogIn:
         self.menu_bar = QFrame(widget)
         self.menu_bar.setGeometry(QRect(0, 0, 800, 250))
         self.menu_bar.setStyleSheet(
-            "color: white; background-color: rgb(249, 138, 15);"
+            "QFrame {"
+            "   background-color: rgb(249, 138, 15);"
+            "}"
+            "QLabel {"
+            "   color: white;"
+            "   background-color: rgba(0, 0, 0, 0);"
+            "   font: 24pt 배달의민족 주아;"
+            "}"
+            "QPushButton {"
+            "   font: 16pt 배달의민족 주아;"
+            "   color: white;"
+            "   background-color: rgba(0, 0, 0, 0);"
+            "   border: none;"
+            "   border-radius: 16px;"
+            "}"
+            "QPushButton:hover {"
+            "   background-color: rgb(241, 219, 194);"
+            "}"
         )
 
-        img_NFC = QPixmap("../../Images/icon_2_2.png")
+        img_NFC = QPixmap(os.path.join(PATH_IMG, "icon_2_2.png"))
         self.button_NFC = QPushButton(self.menu_bar)
-        self.button_NFC.setGeometry(QRect(700, 5, 90, 50))
+        self.button_NFC.setGeometry(QRect(680, 5, 110, 50))
         self.button_NFC.setIcon(QIcon(img_NFC))
         self.button_NFC.setIconSize(QSize(32, 32))
-        self.button_NFC.setText("NFC")
-        self.button_NFC.setStyleSheet(
-            "font: 20pt 배달의민족 주아; "
-            "background-color: rgba(0, 0, 0, 0); "
-            "border-radius: 5px; "
-        )
+        self.button_NFC.setText(" NFC")
 
-        img_register = QPixmap("../../Images/Document.png")
+        img_register = QPixmap(os.path.join(PATH_IMG, "Document.png"))
         self.button_register = QPushButton(self.menu_bar)
-        self.button_register.setGeometry(QRect(10, 5, 90, 50))
+        self.button_register.setGeometry(QRect(10, 5, 110, 50))
         self.button_register.setIcon(QIcon(img_register))
         self.button_register.setIconSize(QSize(32, 32))
-        self.button_register.setText("등록")
-        self.button_register.setStyleSheet(
-            "font: 20pt 배달의민족 주아; "
-            "background-color: rgba(0, 0, 0, 0); "
-            "border-radius: 5px; "
-        )
+        self.button_register.setText(" 등록")
 
         self.label_menu = QLabel(self.menu_bar)
         self.label_menu.setGeometry(QRect(300, 5, 200, 60))
         self.label_menu.setText("번호 입력")
         self.label_menu.setAlignment(Qt.AlignCenter)
-        self.label_menu.setStyleSheet("font: 24pt 배달의민족 주아;")
 
         self.display_number = []
         for i in range(8):
             display = QLabel(self.menu_bar)
             display.setGeometry(QRect(80 * i + (60 if i < 4 else 110), 120, 70, 70))
             display.setStyleSheet(
-                "font: 24pt 배달의민족 주아; "
-                "background-color: rgb(199, 107, 5); "
-                "border-radius: 16px; "
+                "background-color: rgb(199, 107, 5);" "border-radius: 16px;"
             )
             self.display_number.append(display)
 
         self.label_dash = QLabel(self.menu_bar)
         self.label_dash.setGeometry(QRect(390, 145, 20, 20))
-        self.label_dash.setPixmap(QPixmap("../../Images/Stick2.png"))
+        self.label_dash.setPixmap(QPixmap(os.path.join(PATH_IMG, "Stick2.png")))
