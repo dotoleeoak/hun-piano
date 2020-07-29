2a19e80fa204c28a5d2e85d39f86b91ede16152e
Jaemin Choi <1dotolee@gmail.com>
2020-07-29 23:31:30 +0900

    Change variable name to be consistent

--- view/in_use.py
+++ view/in_use.py
@@ -11,12 +11,9 @@ class UiInUse:
     def setupUi(self, widget):
         widget.resize(800, 480)
 
-        # TODO: Rename variables of InUse, LogIn, and SignUp to be consistent
-        # Example: frame => frame_smth, label => label_smth
-
-        self.background_frame = QFrame(widget)
-        self.background_frame.setGeometry(QRect(0, 0, 800, 480))
-        self.background_frame.setStyleSheet(
+        self.frame_main = QFrame(widget)
+        self.frame_main.setGeometry(QRect(0, 0, 800, 480))
+        self.frame_main.setStyleSheet(
             "QFrame {"
             "   background-color: rgb(241, 168, 23);"
             "}"
@@ -26,29 +23,29 @@ class UiInUse:
             "}"
         )
 
-        self.piano_picture = QLabel(self.background_frame)
-        self.piano_picture.setGeometry(QRect(320, 30, 160, 150))
-        self.piano_picture.setPixmap(QPixmap(os.path.join(PATH_IMG, "Piano.png")))
+        self.picture_piano = QLabel(self.frame_main)
+        self.picture_piano.setGeometry(QRect(320, 30, 160, 150))
+        self.picture_piano.setPixmap(QPixmap(PATH_IMG / "Piano.png"))
 
-        self.used_time = QLabel(self.background_frame)
-        self.used_time.setGeometry(QRect(0, 180, 800, 40))
-        self.used_time.setStyleSheet("font-size: 16pt;")
-        self.used_time.setText("00:00:00")
-        self.used_time.setAlignment(Qt.AlignCenter)
+        self.label_time = QLabel(self.frame_main)
+        self.label_time.setGeometry(QRect(0, 180, 800, 40))
+        self.label_time.setStyleSheet("font-size: 16pt;")
+        self.label_time.setText("00:00:00")
+        self.label_time.setAlignment(Qt.AlignCenter)
 
-        self.username = QLabel(self.background_frame)
-        self.username.setGeometry(QRect(0, 230, 800, 41))
-        self.username.setStyleSheet("font-size: 20pt;")
-        self.username.setText("홍길동")
-        self.username.setAlignment(Qt.AlignCenter)
+        self.label_name = QLabel(self.frame_main)
+        self.label_name.setGeometry(QRect(0, 230, 800, 40))
+        self.label_name.setStyleSheet("font-size: 20pt;")
+        self.label_name.setText("홍길동")
+        self.label_name.setAlignment(Qt.AlignCenter)
 
-        self.in_use = QLabel(self.background_frame)
-        self.in_use.setGeometry(QRect(0, 270, 800, 71))
-        self.in_use.setStyleSheet("font-size: 32pt;")
-        self.in_use.setText("사용 중")
-        self.in_use.setAlignment(Qt.AlignCenter)
+        self.label_in_use = QLabel(self.frame_main)
+        self.label_in_use.setGeometry(QRect(0, 270, 800, 70))
+        self.label_in_use.setStyleSheet("font-size: 32pt;")
+        self.label_in_use.setText("사용 중")
+        self.label_in_use.setAlignment(Qt.AlignCenter)
 
-        self.button_quit = QPushButton(self.background_frame)
+        self.button_quit = QPushButton(self.frame_main)
         self.button_quit.setGeometry(QRect(340, 360, 120, 60))
         self.button_quit.setText("종료")
         self.button_quit.setStyleSheet(
